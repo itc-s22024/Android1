702797c793f587cc9407681db954f7831abd762a
s22024 <s22024@std.it-college.ac.jp>
2023-08-08 16:07:59 +0900

    8.1終わり。　とりあえず7章と同じ動きする

--- MenuSmaple/app/src/main/java/jp/ac/it_college/std/s22024/menusmaple/MainActivity.kt
+++ MenuSmaple/app/src/main/java/jp/ac/it_college/std/s22024/menusmaple/MainActivity.kt
@@ -1,11 +1,34 @@
 package jp.ac.it_college.std.s22024.menusmaple
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import jp.ac.it_college.std.s22024.menusmaple.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initList(binding.lvMenu)
+    }
+
+    private fun initList(view: RecyclerView) {
+        view.adapter = MenuListAdapter(teisyokuList, ::order)
+        val layoutManager = LinearLayoutManager(this)
+        view.layoutManager = layoutManager
+        view.addItemDecoration(DividerItemDecoration(this, layoutManager.orientation))
+    }
+
+    private fun order(name: String, price: Int) {
+        startActivity(Intent(this, MenuThanksActivity::class.java).apply {
+            putExtra("menuName", name)
+            putExtra("menuprice", price)
+        })
     }
 }
