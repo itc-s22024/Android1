6086a3c377d438c699b1b71e8ace444ffc74817b
s22024 <s22024@std.it-college.ac.jp>
2023-08-15 16:59:59 +0900

    commit してみた

--- FragmantSample/app/src/main/java/jp/ac/it_college/std/s22024/fragmantsample/MainActivity.kt
+++ FragmantSample/app/src/main/java/jp/ac/it_college/std/s22024/fragmantsample/MainActivity.kt
@@ -2,10 +2,31 @@ package jp.ac.it_college.std.s22024.fragmantsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.os.bundleOf
+import androidx.fragment.app.commit
+import jp.ac.it_college.std.s22024.fragmantsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.setFragmentResultListener(REQUEST_SELECTED_MENU, this, ::onselectedMenu)
+    }
+    private fun onselectedMenu(requestKey: String, bundle: Bundle) {
+        Log.i("SELECTED_MENU", "requestKey: ${requestKey}, bundle: ${bundle}.")
+
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            addToBackStack("Only List")
+            replace(R.id.fragmentMainContainer, MenuThanksFragment ::class.java,bundle)
+            bundleOf(
+                ARG_NAME to bundle.getString(RESULT_NAME, ""),
+                ARG_PRICE to bundle.getInt(RESULT_PRICE, 0)
+            )
+        }
     }
 }
---
+++ FragmantSample/app/src/main/java/jp/ac/it_college/std/s22024/fragmantsample/MenuThanksFragment.kt
@@ -0,0 +1,59 @@
+package jp.ac.it_college.std.s22024.fragmantsample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import jp.ac.it_college.std.s22024.fragmantsample.databinding.FragmentMenuThanksBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+internal const val ARG_NAME = "menuName"
+internal const val ARG_PRICE = "menuPrice"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MenuThanksFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MenuThanksFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var _binding: FragmentMenuThanksBinding? = null
+
+    private val binding get() = _binding!!
+
+    private var param1: String? = null
+    private var param2: String? = null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMenuThanksBinding.inflate(inflater, container, false)
+            return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val menuName = arguments?.getString(ARG_NAME) ?: ""
+        val menuPrice = arguments?.getInt(ARG_PRICE) ?: 0
+
+        binding.tvMenuName.text = menuName
+        binding.tvMenuPrice.text = "%,d".format(menuPrice)
+
+        binding.btthxBack.setOnClickListener(::onBackButtonClick)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun onBackButtonClick(v: View) {
+        parentFragmentManager.popBackStack()
+    }
+
+}
